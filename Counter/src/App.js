9699b35e70bf869e75961ed945b22e2bf7d4056b
Alex Filareti <alex@MacBook-Air-di-Alex.local>
2018-10-16 10:13:19 +0200

    commit1

---
+++ Counter/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+
+class App extends React.Component {
+  
+  state = {
+    counter : 0
+  }
+  
+  
+  render() {
+    return (
+      <div>
+        <h1>Counter: {this.state.counter} </h1>
+        <button onClick = {() => this.setState({counter: this.state.counter + 1 })}>
+          Increment
+        </button>
+        <button onClick = {() => this.setState({counter: this.state.counter - 1 })}>
+          Decrement
+        </button>
+      </div>
+
+    );
+  }
+}
+
+export default App;
